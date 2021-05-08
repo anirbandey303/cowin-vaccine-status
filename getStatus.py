93c17fcc7da1f630b4300c19267c4d0606c95bd2
Anirban Dey <anirbandey303@gmail.com>
2021-05-08 21:34:21 +0530

    Added Telegram Support

--- getStatus.py
+++ getStatus.py
@@ -1,12 +1,19 @@
 from cowin_api import CoWinAPI
 import json
 import time
+import requests
 
 class Cowin:
   def notifyUsers(self):
     # will send notification to users that vaccine is available
     print("Vaccine Found")
-    return
+    with open("telegram.params", 'r') as file:
+      data = file.read()
+    text = "Apollsdsdsdsdsdsdo 11 available"
+    base_url = data + '\"{}\"'.format(text)
+    response = requests.get(base_url)
+    return response
+
   def checkForVaccine(self):
     district_id = '725' # We can make it to pass it as parameters.
     date = '10-05-2021'  # Optional. Takes today's date by default
@@ -21,7 +28,7 @@ class Cowin:
     #print(len(available_centers["centers"]))
     # print(len(available_centers["centers"][0]["sessions"]))
     # print(available_centers["centers"][0]["sessions"][3]["date"])
-    # available_centers["centers"][0]["sessions"][1]["available_capacity"] = 1 
+    # available_centers["centers"][0]["sessions"][1]["available_capacity"] = 1
     
     for i in range(len(available_centers["centers"])):
       print(available_centers["centers"][i]["name"]) # Prints the name of the hospital
@@ -39,5 +46,7 @@ class Cowin:
 obj = Cowin()
 while True:
   obj.checkForVaccine()
-  print("Press 'Ctrl + C' to exit...")
+  named_tuple = time.localtime() # get struct_time
+  time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+  print(time_string," : Press 'Ctrl + C' to exit...")
   time.sleep(60)
